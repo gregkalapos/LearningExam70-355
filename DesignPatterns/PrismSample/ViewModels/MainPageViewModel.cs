5aa7fb7fbceac769abfc707412fddb86743c8864
Gergely Kalapos <gergo@kalapos.net>
2015-10-16 21:44:01 +0200

    ThreadPool sample added

--- DesignPatterns/PrismSample/ViewModels/MainPageViewModel.cs
+++ DesignPatterns/PrismSample/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,15 @@
-﻿using Prism.Events;
+﻿using Prism.Commands;
+using Prism.Events;
 using Prism.Windows.AppModel;
 using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Syndication;
 
 namespace PrismSample.ViewModels
 {
@@ -15,6 +18,8 @@ namespace PrismSample.ViewModels
     {
         private readonly IDataRepository _dataRepository;
         private IEventAggregator _eventAggr;
+
+        public DelegateCommand  DownloadBtnTouched { get; private set; }
 
         public MainPageViewModel(IDataRepository dataRepository, INavigationService navService, IEventAggregator eventAggr)
         {
@@ -25,7 +30,52 @@ namespace PrismSample.ViewModels
             _eventAggr = eventAggr;
 
             //NavigateCommand = new DelegateCommand(() => navService.Navigate("UserInput", null));
+
+            DownloadBtnTouched = new DelegateCommand(async() =>
+            {
+                Windows.Web.Syndication.SyndicationClient client = new SyndicationClient();
+
+                try
+                {
+                    var txt = await MyTestAsyncClass.MyMethodAsync();
+                }
+                catch(Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+
+                // Force the SyndicationClient to download the information.
+                client.BypassCacheOnRetrieve = true;
+
+                Uri feedUri
+                    = new Uri("http://blogs.windows.com/feed/"); //"http://windowsteamblog.com/windows/b/windowsexperience/atom.aspx");
+
+                try
+                {
+                    // Call SyndicationClient RetrieveFeedAsync to download the list of blog posts.
+                    SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+
+                    // The rest of this method executes after await RetrieveFeedAsync completes.
+                    var Text = feed.Title.Text + Environment.NewLine;
+
+                    foreach (SyndicationItem item in feed.Items)
+                    {
+                         Text += item.Title.Text + ", " +
+                                         item.PublishedDate.ToString() + Environment.NewLine;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log Error.
+                    //rssOutput.Text =
+                    //    "I'm sorry, but I couldn't load the page," +
+                    //    " possibly due to network problems." +
+                    //    "Here's the error message I received: "
+                    //    + ex.ToString();
+                }
+            });
         }
+
 
         public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
         {
@@ -41,5 +91,17 @@ namespace PrismSample.ViewModels
 
         [RestorableState]
         public string MainText { get; set; }
+    }
+
+
+    public class MyTestAsyncClass
+    {
+        public static async Task<string> MyMethodAsync()
+        {
+            await Task.Delay(400);
+
+            throw new Exception("test");
+            return "sdgd";
+        }
     }
 }
--- DesignPatterns/PrismSample/Views/LoginPage.xaml.cs
+++ DesignPatterns/PrismSample/Views/LoginPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,68 @@ namespace PrismSample.Views
         public LoginPage()
         {
             this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // if (animatedGrid.Opacity == 0)
+            ShowStoryboard.Begin();
+            //   else
+            // HideStoryboard.Begin();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ulong nOfProme = 9999;
+            int result = 0;
+
+            var m_workItem = Windows.System.Threading.ThreadPool.RunAsync((workitem) =>
+             {
+
+                 ulong primeFound = 0;
+                 int i = 2;
+                
+                 while (primeFound < nOfProme-1)
+                 {
+                     bool prime = true;
+
+                     i++;
+
+                     for (int j = 2; j < i; ++j)
+                     {
+                         if (i % j == 0)
+                         {
+                             prime = false;
+                             break;
+                         }
+                     }
+
+                     if (prime)
+                     {
+                         primeFound++;
+                     
+                         Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                         new DispatchedHandler(() =>
+                         {
+                             PrimeCalculationTextblock.Text = "Primes found: " + primeFound;
+                         }));
+
+                         result = i;
+                     }
+                 }
+
+                
+             });
+
+            m_workItem.Completed = new AsyncActionCompletedHandler((workItem, status) =>
+            {
+                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                        new DispatchedHandler(() =>
+                        {
+                            PrimeCalculationTextblock.Text = "The value is: " + result;
+                        }));
+
+            });
         }
     }
 }
