ef4a5244acc45193b6daa7f73037134f4fd11c26
Gergely Kalapos <gergo@kalapos.net>
2015-10-17 00:28:01 +0200

    Added backgroundtask sample

---
+++ DesignPatterns/TestBackgroundTask/TestBackgroundTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace TestBackgroundTask
+{
+    public sealed class TestBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+
+            taskInstance.GetDeferral().Complete();
+        }
+    }
+}
