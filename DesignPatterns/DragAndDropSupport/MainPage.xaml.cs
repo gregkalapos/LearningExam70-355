fa737a1c73b1ab61e6163e5fd3ce297009757cfa
gergo <gergo@kalapos.net>
2015-10-19 07:59:06 -0700

    Drag and Drop sample improved

--- DesignPatterns/DragAndDropSupport/MainPage.xaml.cs
+++ DesignPatterns/DragAndDropSupport/MainPage.xaml.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,5 +29,37 @@ namespace DragAndDropSupport
         {
             this.InitializeComponent();
         }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var storageItem = await e.DataView.GetStorageItemsAsync();
+                if (storageItem != null)
+                {
+                    var bmImage = new BitmapImage();
+                    bmImage.SetSource(await (storageItem.First() as StorageFile).OpenReadAsync());
+
+                    Image img = new Image();
+                    img.Source = bmImage;
+                    ListViewForItems.Items.Add(img);
+                }               
+            }
+        }
+
+        private async void image1_DragStarting(UIElement sender, DragStartingEventArgs args)
+        {
+            args.Data.RequestedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+
+            var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(@"Assets\Alcatraz.JPG");
+
+            args.Data.SetStorageItems(new List<IStorageItem> { file });
+        }
+
     }
 }
