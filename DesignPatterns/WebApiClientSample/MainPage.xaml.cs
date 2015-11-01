2f5d371ba6f4ccf810cd22861ef9484b033c2cbf
Gergely Kalapos <gergo@kalapos.net>
2015-11-02 00:31:02 +0100

    Webapi client added

---
+++ DesignPatterns/WebApiClientSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebApiClientSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:50007");
+                var resp = await httpClient.GetAsync("api/Stocks/2");
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    var jsonObj = JsonObject.Parse(await resp.Content.ReadAsStringAsync());
+                    var str = jsonObj.ToString();
+
+                }
+            }
+        }
+    }
+}
